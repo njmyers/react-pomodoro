b4f26c25faebd43b3a611e3649317778895be828
njmyers <17010801+njmyers@users.noreply.github.com>
2017-09-15 14:28:33 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import Pomodoro from './Pomodoro';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Pomodoro />, document.getElementById('root'));
-registerServiceWorker();
